dad9a984b0b2214a428afdd9ba8646f608caafe7
danieljjyuen <45666484+danieljjyuen@users.noreply.github.com>
2022-11-05 20:24:14 -0400

    part1 exercises 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,27 @@
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const content = [
-    {
-      part: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      part: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      part: 'State of a component',
-      exercises: 14,
-    },
-  ]
-
+  const course = {  
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content content={content} />
-      <Total content={content} />
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total content={course.parts} />
     </div>
   )
 }
@@ -55,7 +56,7 @@ const Total = ({content}) => {
 
 const Part = ({content}) => {
   return (
-    <div>{content.part} {content.exercises}</div>
+    <div>{content.name} {content.exercises}</div>
   )
 }
 
