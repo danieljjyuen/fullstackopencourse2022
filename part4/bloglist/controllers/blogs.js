703bb2e9455dc5bc7547f1d85ad86c9a8378e8b9
danieljjyuen <danyuenandroid@gmail.com>
2023-08-29 15:18:50 -0400

    part 5 exercises 5.5 - 5.11

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -29,6 +29,7 @@ blogsRouter.post('/', async (request, response, next) => {
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
+    await savedBlog.populate('user', {username:1, name:1})
     response.status(201).json(savedBlog)
   }catch(error){
     next(error)
@@ -38,6 +39,9 @@ blogsRouter.post('/', async (request, response, next) => {
   
 blogsRouter.delete('/:id', async (request, response, next) => {
   const blog = await Blog.findById(request.params.id)
+  console.log('blog is ', blog)
+  console.log('blog.user.tostring  ', blog.user.toString() )
+  console.log('request.user.tostring   ,', request.user.toString())
   if(blog.user.toString() !== request.user.toString()){
     response.status(401).json({error: 'incorrect token'})
   }
@@ -56,11 +60,13 @@ blogsRouter.put('/:id', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url:body.url,
-    likes:body.likes
+    likes:body.likes,
+    user:body.userId
   }
 
   try{
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    await updatedBlog.populate('user',{username:1, name:1})
     response.json(updatedBlog)
   }catch{
     response.status(500).end()
