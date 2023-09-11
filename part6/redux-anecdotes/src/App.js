35d1f15f36719cd392775e0d2e4a1c88205b5f56
danieljjyuen <danyuenandroid@gmail.com>
2023-09-11 18:51:21 -0400

    part 6 exercise 6.14 - 6.15

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,7 +3,15 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import anecdotesService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes, setAnecdotes } from './reducers/anecdoteReducer'
 const App = () => {
+const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  },[])
 
   return (
     <div>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,33 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit"
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import anecdoteService from '../services/anecdotes'
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+// const anecdotesAtStart = [
+//   'If it hurts, do it more often',
+//   'Adding manpower to a late software project makes it later!',
+//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+//   'Premature optimization is the root of all evil.',
+//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+// ]
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+// const getId = () => (100000 * Math.random()).toFixed(0)
 
-const initialState = anecdotesAtStart.map(asObject)
+// const asObject = (anecdote) => {
+//   return {
+//     content: anecdote,
+//     id: getId(),
+//     votes: 0
+//   }
+// }
+
+// const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState:[],
   reducers: {
-    createAnecdote(state, action){ 
-        const content = action.payload
-        return state.concat(asObject(content))
-    },
+    // createAnecdote(state, action){ 
+    //     const content = action.payload
+    //     return state.concat(asObject(content))
+    // },
     voteAnecdote(state, action) {
       const id = action.payload
       const findAnecdote = state.find(state=> state.id === id)
@@ -36,9 +38,29 @@ const anecdoteSlice = createSlice({
         votes: findAnecdote.votes + 1
       }
       return state.map(state => state.id === id ? changedAnecdote : state)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
+    },
+    appendAnecdotes(state,action){
+      return state.concat(action.payload)
     }
   }
 })
 
-export const {createAnecdote,voteAnecdote} = anecdoteSlice.actions
+export const createAnecdote =  (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    console.log(newAnecdote)
+    dispatch(appendAnecdotes(newAnecdote))
+  }
+}
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    await anecdoteService.getAll().then(response => dispatch(setAnecdotes(response)))
+  }
+}
+
+export const {voteAnecdote, setAnecdotes, appendAnecdotes} = anecdoteSlice.actions
 export default anecdoteSlice.reducer
